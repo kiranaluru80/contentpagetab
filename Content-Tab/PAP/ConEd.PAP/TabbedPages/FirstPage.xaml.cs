a0c64bd173525a0fa6547dd08adc9f21cc1d0ba1
kiranaluru80 <31380802+kiranaluru80@users.noreply.github.com>
2018-04-29 18:20:09 -0400

    tab-content

---
+++ Content-Tab/PAP/ConEd.PAP/TabbedPages/FirstPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using ConEd.PAP.Models;
+using Xamarin.Forms;
+
+namespace ConEd.PAP.TabbedPages
+{
+    public partial class FirstPage : ContentPage
+    {
+        public FirstPage(TabPolicyModel policyModel)
+        {
+            InitializeComponent();
+
+            lstDDT.ItemsSource = policyModel.PolicyItems;
+        }
+    }
+}
